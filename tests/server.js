f0afc379f19a0b2b7c9848ada4ffe63aee4da813
Diogo Costa <costa.h4evr@gmail.com>
2011-08-08 23:13:54 +0100

    Fixed test server.

--- tests/server.js
+++ tests/server.js
@@ -3,7 +3,10 @@ var yaml = require('yamlparser');
 
 http.createServer(function(req, res) {
 	res.writeHead(200, {'Content-Type' : 'text/plain' });
-	var data = yaml.parseFile('example.yml');
+	
+	var fs = require('fs');
+	var fileContents = fs.readFileSync('example.yml', 'utf8');
+	var data = yaml.eval(fileContents);
 	res.end(JSON.stringify(data));
 }).listen(8080, "127.0.0.1");
 
